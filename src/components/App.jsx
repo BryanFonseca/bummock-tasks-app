6c9ceaa6cf64c2f2b0d43c6718e9e82b3adde58f
Bryan Fonseca <isaac1805@hotmail.com>
2023-12-27 15:27:47 -0500

    Add base markup & reset styles

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+    return (
+        <main>
+            <div>
+                <button>Left</button>
+                <div>
+                    <span>Lunes</span>
+                    <span>18/05/2023</span>
+                </div>
+                <button>Right</button>
+            </div>
+
+            <div>
+                <input type="text" />
+                <button>Add</button>
+            </div>
+
+            <ul>
+                <li>
+                    <input type="checkbox" />
+                    <span>Salvar al mundo</span>
+                    <button>Delete</button>
+                </li>
+            </ul>
+        </main>
+    );
+}
+
+export default App;
