8312239e65665fe16fafbe4e10737d24ca4af1bf
Bryan Fonseca <isaac1805@hotmail.com>
2023-12-28 12:13:49 -0500

    Mirror master interaction with DateSelector

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,38 +4,73 @@ import TaskList from "./TaskList/TaskList";
 import Task from "./Task/Task";
 import Layout from "./Layout/Layout";
 import { useReducer } from "react";
+import { formatDate, parseDate } from "../helpers/dates";
 
-const INITIAL_STATE = [
-    {
-        id: 1,
-        content: "Salvar al mundo",
-        isCompleted: false,
-    },
-];
+const INITIAL_STATE = {
+    date: formatDate(new Date()),
+    tasks: [
+        {
+            id: 1,
+            content: "Salvar al mundo",
+            isCompleted: false,
+            date: formatDate(new Date()),
+        },
+    ],
+};
 
 function reducer(prevState, action) {
     switch (action.type) {
         case "addTask": {
             const task = action.payload;
-            return [task, ...prevState];
+            return {
+                ...prevState,
+                tasks: [
+                    {
+                        ...task,
+                        date: prevState.date,
+                    },
+                    ...prevState.tasks,
+                ],
+            };
         }
 
         case "deleteTask": {
             const taskId = action.payload;
-            return [...prevState].filter((task) => task.id !== taskId);
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter((task) => task.id !== taskId),
+            };
         }
 
         case "flipCompletion": {
             const taskId = action.payload;
-            const newState = prevState.map((task) =>
-                task.id === taskId
-                    ? {
-                          ...task,
-                          isCompleted: !task.isCompleted,
-                      }
-                    : { ...task }
-            );
-            return newState;
+            return {
+                ...prevState,
+                tasks: prevState.tasks.map((task) =>
+                    task.id === taskId
+                        ? {
+                              ...task,
+                              isCompleted: !task.isCompleted,
+                          }
+                        : { ...task }
+                ),
+            };
+        }
+
+        case "changeDay": {
+            const amount = action.payload;
+            const { date: currentDateString } = prevState;
+            const currentDate = parseDate(currentDateString);
+            return {
+                ...prevState,
+                date: formatDate(
+                    new Date(
+                        currentDate.getFullYear(),
+                        currentDate.getMonth(),
+                        currentDate.getDate() + amount
+                    )
+                ),
+            };
         }
 
         default:
@@ -44,9 +79,9 @@ function reducer(prevState, action) {
 }
 
 function App() {
-    const [tasks, dispatch] = useReducer(reducer, INITIAL_STATE);
+    const [{ tasks, date }, dispatch] = useReducer(reducer, INITIAL_STATE);
 
-    function handleFlipCompletion(taskId, isCompleted) {
+    function handleFlipCompletion(taskId) {
         dispatch({
             type: "flipCompletion",
             payload: taskId,
@@ -63,17 +98,28 @@ function App() {
 
     return (
         <Layout>
-            <DateSelector />
+            <DateSelector
+                onPrevDay={() => dispatch({ type: "changeDay", payload: -1 })}
+                onNextDay={() => dispatch({ type: "changeDay", payload: 1 })}
+                date={parseDate(date)}
+            />
             <AddBar onAdd={handleAdd} />
             <TaskList>
-                {tasks.map((task) => (
-                    <Task
-                        key={task.id}
-                        onFlipCompletion={() => handleFlipCompletion(task.id)}
-                        onDelete={() => handleDelete(task.id)}
-                        {...task}
-                    />
-                ))}
+                {tasks.filter((task) => task.date === date).length === 0 && (
+                    <em>Sin tareas</em>
+                )}
+                {tasks
+                    .filter((task) => task.date === date)
+                    .map((task) => (
+                        <Task
+                            key={task.id}
+                            onFlipCompletion={() =>
+                                handleFlipCompletion(task.id)
+                            }
+                            onDelete={() => handleDelete(task.id)}
+                            {...task}
+                        />
+                    ))}
             </TaskList>
         </Layout>
     );
